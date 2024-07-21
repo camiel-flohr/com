844f7751df0ea1628e434e77d991115115e293c3
camiel-flohr <camielflohr@gmail.com>
2024-07-21 22:46:56 +0000

    commit

--- index.js
+++ index.js
@@ -572,7 +572,7 @@ function runVideos(el){
 
     el.find("video").each(function(){
         const video = this;  
-        console.log(this);
+        console.log(video);
 
         $(this).on("click", function() { 
             if(!$(this).attr("swiperVideo")){
